f137790fe57e71dfd4d2986f1b717af2cc6df2c8
Yeison Berbesi Chapeta <yeison.berbesi@gmail.com>
2026-05-01 16:33:40 -0500

    Fix prims-client

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,16 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+import pkg from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const { PrismaClient } = pkg;
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-})
+});
 
-const prisma = new PrismaClient({adapter});
-
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
