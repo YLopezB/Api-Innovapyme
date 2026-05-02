929d962725c386d1af6e194847ac5bc049f016ba
YLopezB <andislopez13@gmail.com>
2026-05-02 17:40:54 -0500

    fix creacion del admin

--- src/config/scripts/UserTypes.js
+++ src/config/scripts/UserTypes.js
@@ -1,6 +1,8 @@
 import { createManyUsersTypes } from "../../repositories/UserType/createUserType.js";
 import { readUserTypes } from "../../repositories/UserType/readUserType.js";
 import prisma from "../database.js";
+import bcryptjs from "bcryptjs";
+
 
 const userTypes = [
   { nombre: "Administrador",
@@ -14,21 +16,6 @@ const userTypes = [
    },
 ];
 
-const admin = {
-  nombre: "Administrador",
-  apellido: "Principal",
-  correo: "admin@example.com",
-  contrasena: "admin123",
-  telefono: "3123456789"
-};
-
-async (admin) => {
-  return await prisma.usuario.upsert({
-    where: { correo: admin.correo },
-    update: {},
-    create: admin,
-  });
-};
 
 try {
   const existingUserTypes = await readUserTypes({
@@ -47,6 +34,28 @@ try {
   console.log(
     `Seed de tipos de usuario completado. Creados: ${missingUserTypes.length}, ya existentes: ${userTypes.length - missingUserTypes.length}`
   );
+  const admin = {
+  nombre: "Administrador",
+  apellido: "Principal",
+  correo: "admin@example.com",
+  contrasena: "admin123",
+  telefono: "3123456789",
+  id_tipo_usuario: 1,
+};
+
+const hashPass = bcryptjs.hashSync(admin.contrasena, 10);
+admin.contrasena = hashPass;
+
+const createAdminUser = async (admin) => {
+  return await prisma.usuario.upsert({
+    where: { correo: admin.correo },
+    update: {},
+    create: admin,
+  });
+};
+
+await createAdminUser(admin);
+
 } catch (error) {
   console.error("Error ejecutando seed de tipos de usuario:", error);
   process.exitCode = 1;
