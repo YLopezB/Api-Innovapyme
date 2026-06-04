2927af6f42be26d17a6ab35c0c2affa515f0121a
Yeison Berbesi Chapeta <yeison.berbesi@gmail.com>
2026-06-03 19:03:57 -0500

    Add test de autenticación

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
-import express from 'express';
 import dotenvExpand from 'dotenv-expand';
 import dotenv from 'dotenv';
 dotenvExpand.expand(dotenv.config());
-import cors from 'cors';
-import morgan from 'morgan';
-import swaggerUi from 'swagger-ui-express';
-import specs from './config/swagger/swagger.js';
-import routerIndex from './routes/index.js';
-import handlerError from './middlewares/handlerError.js';
 
-const app = express();
+import app from './app.js';
+
 const PORT = process.env.PORT || 3000;
 
-const ready = () => console.log(`Servidor corriendo en el puerto ${PORT}`);
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-app.use(morgan('dev'));
-
-//Swagger config
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
-
-//Ruta api
-app.use('/api', routerIndex);
-
-//Handler de error
-app.use(handlerError);
-
-app.listen(PORT, ready);
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
